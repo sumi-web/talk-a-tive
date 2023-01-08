7a76990aa58aff2145aff01abd4132e68456872b
Sumit Yadav <sumity159@gmail.com>
2023-01-08 18:07:16 +0530

    small correction

--- server1/src/controllers/user.controller.ts
+++ server1/src/controllers/user.controller.ts
@@ -4,7 +4,7 @@ import { UserModel } from '../models/user.model';
 import { AppError, StatusCode } from '../utils/appError';
 
 export const registerUser = asyncHandler(async (req: Request, res: Response) => {
-  const alreadyUserExist = await UserModel.find({ email: req.body.email });
+  const alreadyUserExist = await UserModel.findOne({ email: req.body.email });
 
   if (alreadyUserExist) throw new AppError({ message: `user already exist with email ${req.body.email}`, statusCode: StatusCode.BAD_REQUEST });
 
