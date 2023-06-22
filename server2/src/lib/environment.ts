02465b313cec1623082e4a89da96d5ac79b8314c
Sumit Yadav <sumity159@gmail.com>
2023-06-22 23:36:48 +0530

    changes

--- server2/src/lib/environment.ts
+++ server2/src/lib/environment.ts
@@ -1,16 +1,16 @@
 import fs from 'fs';
 import path from 'path';
 import { config as configDotenv } from 'dotenv';
-import { EnvironmentFile, Environments } from '@/enums/environment.enum';
 import { type CommonEnvKeys } from '@/types/environment.type';
+import { EnvironmentFile, Environments } from '@/enums/environment.enum';
 
 export interface IEnvironment {
   getCurrentEnvironment: () => string;
   setEnvironment: (env?: Environments) => void;
   isProd: () => boolean;
   isDev: () => boolean;
-  isTest: () => boolean;
-  isStage: () => boolean;
+  isTest?: () => boolean;
+  isStage?: () => boolean;
 }
 
 class Environment implements IEnvironment {
@@ -58,4 +58,43 @@ class Environment implements IEnvironment {
     }
     return fs.existsSync(envPath) ? envPath : defaultEnvPath;
   }
+
+  private validateEnvValues() {
+    const env = cleanEnv(process.env, envValidationConfig);
+    this.port = env.PORT;
+    this.appUrl = env.APP_BASE_URL;
+  }
+
+  public setEnvironment(env = Environments.DEV): void {
+    this.env = env;
+
+    const envKey = Object.keys(Environments).find((key) => Environments[key] === this.env) as keyof typeof Environments;
+    const envPath = this.resolveEnvPath(envKey);
+
+    configDotenv({ path: envPath });
+    this.validateEnvValues();
+  }
+
+  public getCurrentEnvironment() {
+    return this.env;
+  }
+
+  public isProd() {
+    return this.env === Environments.PRODUCTION;
+  }
+
+  public isDev() {
+    return this.env === Environments.DEV;
+  }
+
+  // public isStage() {
+  //   return this.env === Environments.STAGING;
+  // }
+
+  // public isTest() {
+  //   return this.env === Environments.TEST;
+  // }
 }
+
+export { Environment };
+export default new Environment();
